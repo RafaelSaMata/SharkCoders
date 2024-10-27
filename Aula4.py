d277faa0e606be69409e773935d27bd5fc94d538
RafaelSaMata <rafaelsamata@gmail.com>
2024-10-27 19:46:44 +0000

    Add files via upload

---
+++ Aula4.py
@@ -0,0 +1,36 @@
+from turtle import *
+
+'''for i in range(50) :
+    forward(10)
+    right(90)
+    forward(10)
+    left(90)'''
+'''
+speed(11)
+size = 10
+for i in range(7):
+    circle(size)
+    size += 10
+right(90)
+size = 10
+for i in range(7):
+    circle(size)
+    size += 10
+right(90)
+size = 10
+for i in range(7):
+    circle(size)
+    size += 10
+right(90)
+size = 10
+for i in range(7):
+    circle(size)
+    size += 10'''
+
+color('black')
+begin_fill()
+for i in range(4) :
+    forward(200)
+    left(90)
+
+exitonclick()
---
+++ Aula5.py
@@ -0,0 +1,56 @@
+from turtle import *
+
+
+'''for i in range(4) :
+    forward(100)
+    left(90)
+
+forward(100)
+penup()
+forward(50)
+pendown()
+circle(5)
+hideturtle()'''
+
+penup()
+goto(-250,0)
+pendown()
+color("yellow")
+
+
+for i in range(5) :
+    forward(50)
+    left(144)
+
+penup()
+forward(100)
+pendown()
+for i in range(5) :
+    forward(50)
+    left(144)
+
+
+penup()
+forward(100)
+pendown()
+for i in range(5) :
+    forward(50)
+    left(144)
+
+penup()
+forward(100)
+pendown()
+for i in range(5) :
+    forward(50)
+    left(144)
+
+penup()
+forward(100)
+pendown()
+for i in range(5) :
+    forward(50)
+    left(144)
+
+exitonclick()
+
+
